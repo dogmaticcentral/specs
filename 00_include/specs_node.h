f4226521949a3f9aef29281b143c2d46db325aa4
Ivana Mihalek <ivana.mihalek@gmail.com>
2013-12-17 00:02:00 +0100

    copyrite

--- 00_include/specs_node.h
+++ 00_include/specs_node.h
@@ -1,3 +1,24 @@
+/*
+This source code is part of specs, an application for
+protein residue conservation scoring.
+Written by Ivana Mihalek.
+Copyright (C) 2007-2013 Ivana Mihalek.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see<http://www.gnu.org/licenses/>.
+
+Contact: ivana.mihalek@gmail.com.
+*/
 # define NODE_PRINT( fptr, node )                      \
     fprintf ( fptr, "id:%3d     type:%3d      value:%10p    parent:%10p    left:%10p  right:%10p \n",\
 	    node ->id,  node ->type, node, node ->parent,  node ->left,  node ->right);\
